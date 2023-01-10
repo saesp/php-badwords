83446d363c75dfe7509daa28f2edee51618fb2bf
esposito.00@outlook.it <esposito.00@outlook.it>
2023-01-10 19:13:29 +0100

    phar

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+        $phar = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem esse enim labore, aperiam soluta maxime! Asperiores at pariatur, a quisquam deserunt dolor quaerat, officiis ut eaque sapiente neque veniam ducimus?";    
+        $lng = strlen($phar);
+
+        $badword = $_GET['bd'];
+
+        $safePhar = str_replace($badword, "*", $phar);
+        $safeLng = strlen($safePhar);
+    ?>
+</head>
+
+<body>
+</body>
+</html>
